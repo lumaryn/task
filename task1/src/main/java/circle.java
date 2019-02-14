e316b05f48bbf7b4e50a9ec451349e35e763d7e9
pochter123 <pochter123@gmail.com>
2019-02-14 14:29:34 +0300

    seventh commit

---
+++ task1/src/main/java/circle.java
@@ -0,0 +1,13 @@
+public class circle {
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x < 51) {
+
+            System.out.println(x);
+
+            x++;
+
+        }
+    }
+}
