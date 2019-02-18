c26798b6dc9df21bcf84601ea3c042925306f397
pochter123 <pochter123@gmail.com>
2019-02-18 18:15:52 +0300

    eigth commit

--- task1/src/main/java/average.java
+++ task1/src/main/java/average.java
@@ -5,17 +5,15 @@ public class average {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("input 3 numbers");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int sum = a + b + c;
-        if (sum >= 3 && sum % 3 == 0) {
-            int avr = (a + b + c) / 3;
-            System.out.println("average=" + avr);
-            int half_avr = avr / 2;
-            if (half_avr > 3 && avr % 2 == 0) {
-                System.out.println("Программа выполнена корректно");
+        float a = sc.nextInt();
+        float b = sc.nextInt();
+        float c = sc.nextInt();
+        float avr = (a + b + c) / 3;
+        System.out.println("average=" + avr);
+        float half_avr = avr / 2;
+        if (half_avr > 3){
+            System.out.println("Программа выполнена корректно");
             }
         }
     }
-}
+
