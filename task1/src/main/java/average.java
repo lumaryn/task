455a7f92d4a788d9de3f3d2fd3dfbb4396beaeb0
pochter123 <pochter123@gmail.com>
2019-02-18 11:56:03 +0300

    proverki na celoe commit

--- task1/src/main/java/average.java
+++ task1/src/main/java/average.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class average {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,11 +9,11 @@ public class average {
         int b = sc.nextInt();
         int c = sc.nextInt();
         int sum = a + b + c;
-        if (sum >= 3) {
+        if (sum >= 3 && sum % 3 == 0) {
             int avr = (a + b + c) / 3;
             System.out.println("average=" + avr);
             int half_avr = avr / 2;
-            if (half_avr > 3) {
+            if (half_avr > 3 && avr % 2 == 0) {
                 System.out.println("Программа выполнена корректно");
             }
         }
