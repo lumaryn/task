eca73db3f1f2b7932cafa1a8bd523b9da577db25
pochter123 <pochter123@gmail.com>
2019-02-19 14:16:04 +0300

    exercise 11 commit

---
+++ task1/src/main/java/ex11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ex11 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        String st = "";
+        System.out.println("input your text");
+        st = sc.nextLine();
+        System.out.println(st);
+    }
+}
