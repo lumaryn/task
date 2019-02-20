e636684a421e15396619b74224bbfe485fe15cb6
pochter123 <pochter123@gmail.com>
2019-02-20 10:48:57 +0300

    add float commit

--- task1/src/main/java/average.java
+++ task1/src/main/java/average.java
@@ -5,9 +5,9 @@ public class average {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("input 3 numbers");
-        float a = sc.nextInt();
-        float b = sc.nextInt();
-        float c = sc.nextInt();
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
         float avr = (a + b + c) / 3;
         System.out.println("average=" + avr);
         float half_avr = avr / 2;
